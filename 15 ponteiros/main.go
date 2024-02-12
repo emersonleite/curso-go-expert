3ef35e789b3d36ae1c326555522997745f448c00
Emerson Leite <emerson.bl@gmail.com>
2024-02-11 22:02:33 -0300

    chore: ponteiros

--- 15 ponteiros/main.go
+++ 15 ponteiros/main.go
@@ -4,20 +4,32 @@ import "fmt"
 
 func main() {
 
-	var a int
+	//var a int
 
-	a = 10
+	a := 10
 
 	/* Mostra o endereço de memória através de '&' */
 	fmt.Println(&a)
 
-	/* * para se referir a ponteiro */
+	/* asterisco '*' para se referir a ponteiro */
 	var ponteiro *int = &a
 
+	// Mostrando endereço na memória de ponteiro
 	fmt.Println(ponteiro)
 
-	/* Mudando o valor do ponteiro de 10 para 21 */
+	// Outra forma de criar um ponteiro sem definir o tipo explicitamente
+	ponteiro2 := &a
+
+	// Mostrando endereço na memória de ponteiro2
+	fmt.Println(ponteiro2)
+
+	/* Mudando o valor do ponteiro de 10 para 21 e depois, através o ponteiro2 para 22 */
 	*ponteiro = 21
 
-	println(a)
+	*ponteiro2 = 22
+
+	println("valor de a = ", a)
+
+	/* Para mostrar o valor do ponteiro utiliza-se o asterisco '*' */
+	fmt.Println("valor de ponteiro = ", *ponteiro)
 }
